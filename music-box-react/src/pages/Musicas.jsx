54e84c4d06ad7123c0f016433bcbe537d80a1dd1
IvanCCO <ivan.miranda@sptech.school>
2022-04-11 21:09:37 -0300

    music box, com a ajuda da Yumi

---
+++ music-box-react/src/pages/Musicas.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState} from "react";
+import React from "react";
+import ItemMusica from '../components/ItemMusica';
+import Menu from "../components/Menu"; 
+
+import api from '../api';
+
+function Musicas() {
+
+ const[musicas, setMusicas] = useState([]);
+
+  useEffect(() => {
+  
+    api.get().then((res) =>{
+      setMusicas(res.data);
+    }).catch((err) =>{
+      console.log(err);
+    })
+
+  },[])
+ 
+  return (
+    <>
+      <Menu />
+  
+      <div className="container">
+        <div className="filter">
+          <button className="btn">Adicionar</button>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="music-boxes">
+
+          {
+            musicas.map(musica =>(
+
+              <ItemMusica 
+               musica={musica.musica}
+               artista={musica.artista}
+               genero={musica.categoria}
+               ano={musica.ano}
+               id={musica.id}
+               key={musica.id}
+               />
+
+            ))
+          }
+          
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Musicas;
